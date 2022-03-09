d0bcf2a465fb877f69feb975fbae1b1d47b0a7d9
nguyen.t.hoang-1@ou.edu <nguyen.t.hoang-1@ou.edu>
2022-03-08 20:12:50 -0600

    change

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//const ejs=require('ejs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const res = require('express/lib/response');
 
-var app = express();
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/data');
+
+const app = express();
+
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastNameName: String
+});
+var User = mongoose.model("User", nameSchema);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname)); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -38,17 +54,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.get('/index1.ejs',(request,response)=>{
- // response.sendFile(path.join(__dirname,"/index.html"));
-  //response.sendFile(path.join(__dirname,"index1.ejs"));
-
-  res.send('hello')
-
-
-
-})
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const app = express();
+const createError = require('http-errors');
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.ejs', { title: 'Express' });
+});
+router.get('/index1.ejs', function(req, res, next) {
+  res.render('index1', { title: 'Express' });
+});
+router.get('/login.ejs', function(req, res, next) {
+  res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
