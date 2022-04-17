3b4558962fedfcfab6b7426bd52fdbe4c155ce50
nguyen.t.hoang-1@ou.edu <nguyen.t.hoang-1@ou.edu>
2022-04-17 16:31:39 -0500

    finished menu and reservation

--- public/javascripts/script1.js
+++ public/javascripts/script1.js
@@ -5,16 +5,13 @@ $(function(){
         contentType: 'application/json',     //type of json
         success: function(response){         //when suceess that function below is append these field and button in web
           console.log(response);
-          var tbodyEl=$('tbody');             //using tbody to append
-          tbodyEl.html(''); 
+          var body=$('body');             //using tbody to append
+          body.html('');
+          body.append('MENU TODAY');
+         // body.append('Number           Name               Price                 Kind') ;
           response.menus.forEach(function(menus){          
-            tbodyEl.append('\
-            <tr>\
-            <td class="id" >' +menus.id+'</td>\
-            <td><input type="text" class="name" value= "'+menus.name+ '"></td>\
-            <td><input type="text" class="price" value= "'+menus.price+ '"></td>\
-            <td><input type="text" class="kind" value="'+menus.kind+ '"></td>\
-            <td>\
+            body.append('\
+            <h1>' +menus.id+'.    '+menus.name    +'   $'+menus.price        + '    '   +menus.kind+ '</h1>\
                   ');
           });   
         }
